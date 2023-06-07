db9c635cff3f274a75134ac08da6fe0e05ee9bdc
Lee Hyeonsik <hyeon5819@gmail.com>
2023-06-08 03:07:24 +0900

    feat: emoticon temp CRUD / logintemp

---
+++ static/js/apitemp.js
@@ -0,0 +1,201 @@
+let token = localStorage.getItem("access")
+
+// 현재 로그인 유저
+let payload = localStorage.getItem("payload");
+let payload_parse = JSON.parse(payload);
+let current_user = payload_parse.username;
+// console.log(payload, payload_parse, current_user)
+console.log("현재 로그인한//" + current_user)
+
+// 이메일 유효성 검사
+function CheckEmail(str) {
+    var reg_email = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+    if (!reg_email.test(str)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// 회원가입
+async function handleSignin() {
+    // try {
+    const email = document.getElementById("email").value
+    const email_ = document.getElementById("email")
+    const username = document.getElementById("username").value
+    const password1 = document.getElementById("password1").value
+    const password2 = document.getElementById("password2").value
+    console.log(email_, email, username, password1, password2)
+
+    if (password2 !== password1) {
+        alert("비번 잘못된입력입니다. 확인해주세요.")
+        window.location.reload()
+    } else if (!email || !username || !password1 || !password2) {
+        alert("공란 잘못된입력입니다. 확인해주세요.")
+        window.location.reload()
+    } else if (!CheckEmail(email)) { // 존재한다면 -1이 아닌 숫자가 반환됨
+        alert("이메일 형식이 아닙니다.");
+        email_.focus();
+        console.log(email_)
+        return false;
+    }
+
+    const response = await fetch(`${back_base_url}/users/dj-rest-auth/registration/`, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            "email": email,
+            "username": username,
+            "password1": password1,
+            "password2": password2
+        })
+
+    })
+    let result = await response.json();
+    console.log(result)
+    console.log(JSON.stringify(result))
+
+    if (response.status == 201) {
+        alert("이메일을 확인해주세요.")
+        window.location.reload()
+    } else {
+        console.log(response.status)
+        alert(JSON.stringify(result))
+        window.location.reload()
+    }
+
+}
+
+// 이메일 인증 재전송
+async function handleEmailValify() {
+    const email = document.getElementById("email").value
+    console.log(email)
+
+    const response = await fetch(`${back_base_url}/users/dj-rest-auth/registration/resend-email/`, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            "email": email,
+        })
+    })
+    console.log(response)
+    let result = await response.json();
+    console.log(result)
+    console.log(JSON.stringify(result))
+
+    if (response.status == 200) {
+        alert("이메일을 확인해주세요.")
+        window.location.replace(`${front_base_url}/templates/logintemp.html`)
+    } else {
+        console.log(response.status)
+        alert(JSON.stringify(result))
+        window.location.reload()
+    }
+
+}
+
+// 로그인
+async function handleLogin() {
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+    console.log(username, password)
+
+    const response = await fetch(`${back_base_url}/users/api/token/`, {
+        headers: {
+            "Content-Type": "application/json",
+
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+    })
+
+    const result = await response.json()
+
+    console.log(result)
+
+    if (response.status == 200) {
+        localStorage.setItem("access", result.access);
+        localStorage.setItem("refresh", result.refresh);
+
+        const base64Url = result.access.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        localStorage.setItem("payload", jsonPayload);
+        alert("로그인되었습니다.")
+        window.location.replace(`${front_base_url}/index.html`)
+    } else {
+        console.log(response.status)
+        alert(JSON.stringify(result))
+        window.location.reload()
+    }
+
+}
+
+// 로그아웃
+async function handleLogout() {
+    const response = await fetch(`${back_base_url}/users/dj-rest-auth/logout/`, {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        },
+        method: 'POST',
+    })
+    alert("로그아웃!")
+    localStorage.removeItem("access")
+    localStorage.removeItem("refresh")
+    localStorage.removeItem("payload")
+    location.reload();
+}
+
+// 비번변경-로그인된 상태에서
+async function pschange() {
+    const password1 = document.getElementById("password1").value
+    const password2 = document.getElementById("password2").value
+    console.log(password1, password2)
+
+    if (password2 !== password1) {
+        alert("비번 잘못된입력입니다. 확인해주세요.")
+        window.location.reload()
+    } else if (!password1 || !password2) {
+        alert("공란 잘못된입력입니다. 확인해주세요.")
+        window.location.reload()
+    }
+
+    const response = await fetch(`${back_base_url}/users/dj-rest-auth/password/change/`, {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            "Content-Type": "application/json",
+
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            "new_password1": password1,
+            "new_password2": password2
+        })
+    })
+
+    const result = await response.json()
+    console.log(result)
+
+    if (response.status == 200) {
+        console.log(response)
+        alert("비밀번호가 변경되었습니다.")
+        window.location.replace(`${front_base_url}/index.html`)
+    } else {
+        console.log(result)
+        console.log(response.status)
+        alert(JSON.stringify(result))
+        window.location.reload()
+    }
+
+}
+
--- static/js/emoticon_templist.js
+++ static/js/emoticon_templist.js
@@ -1,40 +1,65 @@
-// if (!localStorage.getItem("access")) {
-//     alert("로그인이 필요합니다.")
-//     window.location.href = `${front_base_url}/templates/login.html`
-// }
+if (!localStorage.getItem("access")) {
+    alert("로그인이 필요합니다.")
+    window.location.href = `${front_base_url}/templates/logintemp.html`
+}
 
 window.onload = async function () {
-    // const access = localStorage.getItem("access");
+    const access = localStorage.getItem("access");
 
     const response = await fetch(`${back_base_url}/emoticons/temp/`, {
-        // headers: {
-        //     Authorization: `Bearer ${access}`,
-        // },
+        headers: {
+            Authorization: `Bearer ${access}`,
+        },
         method: "GET",
     });
 
     if (response.status == 200) {
         response_json = await response.json();
 
-        let emoticons = document.querySelector('#emoticons')
-        let emoticonHtml = ``
+        let emoticons = document.getElementById('emoticons')
 
-        for (let i = 1; i < await response_json.length; i++) {
-            let emoticon = response_json[i]
-            let mainImage = emoticon.images[0].image
-            emoticonHtml += `
-            <div class="col">
-                <div class="card">
-                    <img src="${back_base_url}${mainImage}" class="card-img-top" style="height: 200px; object-fit: cover;">
-                    <div class="card-body">
-                        <h5 class="card-title">${emoticon.title}</h5>
-                        <button onclick="location.href='${front_base_url}/templates/emoticon_detail.html?emoticon_id=${emoticon.id}'">보러가기</button>
-                    </div>
-                </div>
-            </div>
-            `
-        }
-        emoticons.innerHTML = emoticonHtml
+        response_json.forEach(element => {
+            let col = document.createElement('div')
+            col.setAttribute('class', 'col')
+
+            let card = document.createElement('div')
+            card.setAttribute('class', 'card')
+
+            let emoticonImage = document.createElement('img')
+            if (element.images.length == 0) {
+                let mainImage = ''
+                emoticonImage.setAttribute('src', `${back_base_url}${mainImage}`)
+            } else {
+                let mainImage = element.images[0].image
+                emoticonImage.setAttribute('src', `${back_base_url}${mainImage}`)
+            }
+            emoticonImage.setAttribute('class', 'card-img-top')
+            emoticonImage.setAttribute('style', 'height: 200px; object-fit: cover;')
+
+            let cardBody = document.createElement('div')
+            cardBody.setAttribute('class', 'card-body')
+
+            let emoticonTitle = document.createElement('h5')
+            emoticonTitle.setAttribute('class', 'card-title')
+            emoticonTitle.innerText = element.title
+
+            let emoticonCreator = document.createElement('p')
+            emoticonCreator.setAttribute('class', 'card-text')
+            emoticonCreator.innerText = '제작자: ' + element.creator_name
+
+            let detailButton = document.createElement('button')
+            detailButton.setAttribute('onclick', `location.href='${front_base_url}/templates/emoticon_temp_detail.html?emoticon_id=${element.id}'`)
+            detailButton.innerText = '보러가기'
+
+            emoticons.appendChild(col)
+            col.appendChild(card)
+            card.appendChild(emoticonImage)
+            card.appendChild(cardBody)
+            cardBody.appendChild(emoticonTitle)
+            cardBody.appendChild(emoticonCreator)
+            cardBody.appendChild(detailButton)
+        });
+
     } else {
         alert(response.status);
     }

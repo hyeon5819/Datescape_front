fc69d62f9f6c99e0b2d6b9a8f65f611d1b12c596
jmpop <pjm970128@gmail.com>
2023-06-13 21:53:33 +0900

    feat: s3 img

--- components/urls.js
+++ components/urls.js
@@ -1,9 +1,9 @@
 const back_base_url = `http://127.0.0.1:8000`
-
+//https://back.datescape.shop
 const front_base_url = `http://127.0.0.1:5500`
-
+//https://datescape.shop
 // 배포
 const image_url = ``
-
+//
 // 로컬
 // const image_url = `http://127.0.0.1:8000`

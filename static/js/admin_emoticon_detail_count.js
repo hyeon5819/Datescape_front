b2c061152aab7ab1cf061cd222c075cd5a8a2b52
Lee Hyeonsik <hyeon5819@gmail.com>
2023-06-15 15:39:13 +0900

    feat: admin page / admin confirm

--- static/js/admin_emoticon_detail_count.js
+++ static/js/admin_emoticon_detail_count.js
@@ -1,6 +1,20 @@
 const urlParams = new URLSearchParams(window.location.search);
 const emoticonId = urlParams.get("emoticon_id");
 
+// 관리자 유저인지 확인
+if (!localStorage.getItem("access")) {
+    alert("로그인이 필요합니다.")
+    window.location.href = `${front_base_url}/templates/logintemp.html`
+} else{
+    const payload = localStorage.getItem("payload");
+    const payloadParse = JSON.parse(payload)
+
+    if (payloadParse.is_admin == false){
+        alert("관리자만 접근 가능합니다.")
+        window.location.href = `${front_base_url}/`
+    }
+}
+
 const userId = JSON.parse(localStorage.getItem("payload")).user_id;
 
 // 이모티콘 가져오기
@@ -52,32 +66,9 @@ function totalAmount(emoticon) {
     amountTr.appendChild(priceTd)
     amountTr.appendChild(countTd)
     amountTr.appendChild(saleAmountTd)
-
-
-    // // 판매기록 전부 다 가져와서 판매 년 월 값 가져오기
-    // const yearDiv = document.getElementById('year_pagenation')
-    // const monthDiv = document.getElementById('month_pagenation')
-
-    // let soldYear = []
-    // let soldMonth = []
-
-    // for (let i = 0; i < emoticon.sold_count.length; i++) {
-    //     console.log('년도',emoticon.sold_count[i].created_at.split('-')[0])
-    //     console.log('월',emoticon.sold_count[i].created_at.split('-')[1])
-
-    //     if(soldYear.includes(emoticon.sold_count[i].created_at.split('-')[0])){
-    //     }else {
-    //         soldYear.push(emoticon.sold_count[i].created_at.split('-')[0])
-    //     }
-
-    //     if(soldMonth.includes(emoticon.sold_count[i].created_at.split('-')[1])){
-    //     }else {
-    //         soldMonth.push(emoticon.sold_count[i].created_at.split('-')[1])
-    //     }
-    // }
-    // // 기록조회 버튼 넣어주기
-    // console.log(soldYear)
-    // console.log(soldMonth)
+    
+    const tempTr = document.createElement('tr')
+    amountTbody.appendChild(tempTr)
 }
 
 
@@ -112,45 +103,50 @@ window.onload = async function () {
         // 월별 판매량
         let sellNum = 0
 
-        for (let i = 0; i < response.sold_count.length; i++) {
-            console.log('년도', response.sold_count[i].created_at.split('-')[0])
-            console.log('월 int변환', parseInt(response.sold_count[i].created_at.split('-')[1]))
+        let alertStatus = ""
 
+        for (let i = 0; i < response.sold_count.length; i++) {
             if (yearInput.value == response.sold_count[i].created_at.split('-')[0]) {
                 if (monthInput.value == parseInt(response.sold_count[i].created_at.split('-')[1])) {
                     sellNum += 1
                 } else {
-                    alert('해당 월 기록 없음!')
+                    alertStatus = "month_null"
                 }
             } else {
-                alert('해당 년도 기록 없음!')
+                alertStatus = "year_null"
             }
-            console.log(sellNum)
         }
+        if (alertStatus == "year_null"){
+            alert('해당 년도 기록 없음!')
+        } else if (alertStatus == "month_null"){
+            alert('해당 월 기록 없음!')
+        } else {
+            //
+            const amountTbody = document.getElementById('amount_div')
 
-        //
-        const amountTbody = document.getElementById('amount_div')
+            amountTbody.childNodes[3].remove()
 
-        const amountTr = document.createElement('tr')
+            const amountTr = document.createElement('tr')
 
-        const monthTd = document.createElement('td')
+            const monthTd = document.createElement('td')
 
-        monthTd.innerText = yearInput.value + '/' + monthInput.value
+            monthTd.innerText = yearInput.value + '/' + monthInput.value
 
-        const priceTd = document.createElement('td')
-        priceTd.innerText = response.price
+            const priceTd = document.createElement('td')
+            priceTd.innerText = response.price
 
-        const countTd = document.createElement('td')
-        countTd.innerText = response.sold_count.length
+            const countTd = document.createElement('td')
+            countTd.innerText = response.sold_count.length
 
-        const saleAmountTd = document.createElement('td')
-        saleAmountTd.innerText = response.price * response.sold_count.length
+            const saleAmountTd = document.createElement('td')
+            saleAmountTd.innerText = response.price * response.sold_count.length
 
-        amountTbody.appendChild(amountTr)
-        amountTr.appendChild(monthTd)
-        amountTr.appendChild(priceTd)
-        amountTr.appendChild(countTd)
-        amountTr.appendChild(saleAmountTd)
+            amountTbody.appendChild(amountTr)
+            amountTr.appendChild(monthTd)
+            amountTr.appendChild(priceTd)
+            amountTr.appendChild(countTd)
+            amountTr.appendChild(saleAmountTd)
+        }
     })
 
 }

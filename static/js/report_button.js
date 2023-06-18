2cb10cae74632aaafeee885a2f16608918b43e33
jmpop <pjm970128@gmail.com>
2023-06-18 13:34:38 +0900

    feat: article report

--- static/js/report_button.js
+++ static/js/report_button.js
@@ -1,3 +1,4 @@
-async function Report(type, comment_id) {
+async function Report_button(type, comment_id) {
     window.open(location.origin + `/templates/report.html?type=${type}&id=${comment_id}&/`, "Child", "width=1300, height=1000, top=50, left=50")
 }
+

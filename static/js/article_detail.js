4ff414feae49c79edd6d39fb080767a82307f251
heyrin1998 <qq5200@naver.com>
2023-06-23 17:46:59 +0900

    feat: slide 작동 수정

--- static/js/article_detail.js
+++ static/js/article_detail.js
@@ -283,6 +283,30 @@ async function loadArticleList(location) {
         `
     });
 
+    swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+        allowTouchMove: false,
+    });
+
 }
 
 
